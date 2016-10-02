448807a71bb2d641d705d9df2adb46408c0ffd53
Briane Cornish <brianecknight@gmail.com>
2016-10-02 00:45:51 -0400

    Update homeController.js

--- partners/js/home/homeController.js
+++ partners/js/home/homeController.js
@@ -20,3 +20,13 @@
     }
     
 })();
+
+$("#tabs").tabs({
+    activate: function (event, ui) {
+        var active = $('#tabs').tabs('option', 'active');
+        $("#tabid").html('the tab id is ' + $("#tabs ul>li a").eq(active).attr("href"));
+
+    }
+}
+
+);
