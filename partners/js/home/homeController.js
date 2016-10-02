9e0add057ff2126229d441adb22dfb9231b55473
Briane Cornish <brianecknight@gmail.com>
2016-10-02 00:30:29 -0400

    Update homeController.js

--- partners/js/home/homeController.js
+++ partners/js/home/homeController.js
@@ -20,19 +20,3 @@
     }
     
 })();
-
-(function () {
-    var app = angular.module('partner', []);
-
-    app.controller('TabController', function () {
-        this.tab = 1;
-
-        this.setTab = function (tabId) {
-            this.tab = tabId;
-        };
-
-        this.isSet = function (tabId) {
-            return this.tab === tabId;
-        };
-    });
-})();
