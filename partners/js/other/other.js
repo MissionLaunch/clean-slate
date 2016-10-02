e34c4b91d0c1cfc9c10385e6b36f1b19366d568a
Briane Cornish <brianecknight@gmail.com>
2016-10-01 23:10:17 -0400

    Update other.js

--- partners/js/other/other.js
+++ partners/js/other/other.js
@@ -1,1 +1,8 @@
 
+function searchPage($scope){
+    $scope.searchResults = [{label:"Other Statutes",_infoLink:"http://codefordc.org/clean-slate/#/eligibility/50"}];
+    
+    $scope.itemDetail = function(link){
+        $scope.detailFrame = link;
+    };
+}
